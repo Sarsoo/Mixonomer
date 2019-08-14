c8e7d752acc56717209acdb7c481cbbca00c27ae
aj <andrewjpack@gmail.com>
2019-08-14 22:57:58 +0100

    fixed playlist creation id handling

--- spotify/api/spotify.py
+++ spotify/api/spotify.py
@@ -20,7 +20,12 @@ def create_playlist(username, name):
                            user_dict['access_token'],
                            user_dict['refresh_token']))
 
-        net.create_playlist(net.user.username, name)
+        resp = net.create_playlist(net.user.username, name)
+
+        if 'id' in resp:
+            return resp['id']
+        else:
+            raise Exception('Error creating playlist')
 
     else:
         raise ValueError('no/multiple username(s)')
