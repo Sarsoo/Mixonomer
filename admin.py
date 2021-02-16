7dfc2ec0e120ac9abcb0d2f51efac19f75235b50
andy <andy@sarsoo.xyz>
2021-02-16 22:51:13 +0000

    update dependencies, rejigged admin script, better structured ci/cd

--- admin.py
+++ admin.py
@@ -13,52 +13,67 @@ css_rel_path = Path('build', 'style.css')
 folders_to_ignore = ['venv', 'docs', '.git', '.idea', 'node_modules']
 
 
+"""
+COMPONENTS:
+
+* App Engine
+* Cloud Functions:
+    run_user_playlist
+    update_tag
+
+    run_all_playlists
+    run_all_playlist_stats
+    run_all_tags
+
+"""
+
+
 class Admin(Cmd):
     intro = 'Music Tools Admin... ? for help'
     prompt = '> '
 
     locals = ['spotframework', 'fmframework', 'spotfm']
 
-    def prepare_stage(self):
-        print('>> freezing dependencies')
-        self.export_filtered_dependencies()
-
+    def do_prepare_local_stage(self, args):
+        """
+        Prepare local working directory for deployment using static sarsoolib injections
+        """
         # Now done via online repos, not static injection
-        # print('>> backing up a directory')
-        # os.chdir(Path(__file__).absolute().parent.parent)
+        print('>> backing up a directory')
+        os.chdir(Path(__file__).absolute().parent.parent)
 
-        # print('>> deleting old deployment stage')
-        # shutil.rmtree(stage_dir, ignore_errors=True)
+        print('>> deleting old deployment stage')
+        shutil.rmtree(stage_dir, ignore_errors=True)
 
-        # print('>> copying main source')
-        # shutil.copytree('playlist-manager' if Path('playlist-manager').exists() else 'Music-Tools',
-        #                 stage_dir,
-        #                 ignore=lambda path, contents:
-        #                     contents if any(i in Path(path).parts for i in folders_to_ignore) else []
-        #                 )
+        print('>> copying main source')
+        shutil.copytree('playlist-manager' if Path('playlist-manager').exists() else 'Music-Tools',
+                        stage_dir,
+                        ignore=lambda path, contents:
+                            contents if any(i in Path(path).parts for i in folders_to_ignore) else []
+                        )
 
-        # for dependency in Admin.locals:
-        #     print(f'>> injecting {dependency}')
-        #     shutil.copytree(
-        #         Path(dependency, dependency),
-        #         Path(stage_dir, dependency)
-        #     )
+        for dependency in Admin.locals:
+            print(f'>> injecting {dependency}')
+            shutil.copytree(
+                Path(dependency, dependency),
+                Path(stage_dir, dependency)
+            )
 
-        # os.chdir(stage_dir)
+        os.chdir(stage_dir)
+
+# ADMIN
+
+    def do_set_project(self, args):
+        """
+        Set project setting in gcloud console
+        """
+        print('>> setting project')
         subprocess.check_call('gcloud config set project sarsooxyz', shell=True)
 
-    def prepare_frontend(self):
-        print('>> building css')
-        subprocess.check_call(f'sass --style=compressed {str(scss_rel_path)} {str(css_rel_path)}', shell=True)
-
-        print('>> building javascript')
-        subprocess.check_call('npm run build', shell=True)
-
-    def prepare_main(self, path):
-        print('>> preparing main.py')
-        shutil.copy(f'main.{path}.py', 'main.py')
-
     def deploy_function(self, name, timeout: int = 60, region='europe-west2'):
+        """
+        Deploy function with required environment variables
+        """
         subprocess.check_call(
             f'gcloud functions deploy {name} '
             f'--region {region} '
@@ -68,91 +83,10 @@ class Admin(Cmd):
             f'--timeout={timeout}s', shell=True
         )
 
-    def do_all(self, args):
-        # self.prepare_frontend()
-        self.prepare_stage()
-
-        self.prepare_main('api')
-        print('>> deploying api')
-        subprocess.check_call('gcloud app deploy', shell=True)
-
-        self.prepare_main('update_tag')
-        print('>> deploying tag function')
-        self.deploy_function('update_tag')
-
-        self.prepare_main('run_playlist')
-        print('>> deploying playlist function')
-        self.deploy_function('run_user_playlist')
-
-    def do_api(self, args):
-        self.prepare_frontend()
-
-        self.prepare_stage()
-        self.prepare_main('api')
-
-        print('>> deploying')
-        subprocess.check_call('gcloud app deploy', shell=True)
-
-    def do_tag(self, args):
-        self.prepare_stage()
-        self.prepare_main('update_tag')
-
-        print('>> deploying')
-        self.deploy_function('update_tag')
-
-    def do_playlist(self, args):
-        self.prepare_stage()
-        self.prepare_main('run_playlist')
-
-        print('>> deploying')
-        self.deploy_function('run_user_playlist')
-
-    # all playlists cron job
-    def do_playlist_cron(self, args):
-        self.prepare_stage()
-        self.prepare_main('cron')
-
-        print('>> deploying')
-        self.deploy_function('run_all_playlists')
-
-    # all stats refresh cron job
-    def do_playlist_stats(self, args):
-        self.prepare_stage()
-        self.prepare_main('cron')
-
-        print('>> deploying')
-        self.deploy_function('run_all_playlist_stats')
-
-    # all tags cron job
-    def do_tags_cron(self, args):
-        self.prepare_stage()
-        self.prepare_main('cron')
-
-        print('>> deploying')
-        self.deploy_function('run_all_tags')
-
-    # redeploy all cron job functions
-    def do_all_cron(self, args):
-        self.prepare_stage()
-        self.prepare_main('cron')
-
-        print('>> deploying playlists')
-        self.deploy_function('run_all_playlists')
-        print('>> deploying stats')
-        self.deploy_function('run_all_playlist_stats')
-        print('>> deploying tags')
-        self.deploy_function('run_all_tags')
-
-    def do_exit(self, args):
-        exit(0)
-
-    def do_sass(self, args):
-        subprocess.check_call(f'sass --style=compressed {str(scss_rel_path)} {str(css_rel_path)}', shell=True)
-
-    def do_watchsass(self, args):
-        subprocess.check_call(f'sass --style=compressed --watch {str(scss_rel_path)} {str(css_rel_path)}', shell=True)
-
     def do_rename(self, args):
+        """
+        Rename playlist in firestore
+        """
         from music.model.user import User
         from music.model.playlist import Playlist
 
@@ -172,10 +106,94 @@ class Admin(Cmd):
         playlist.name = new_name
         playlist.update()
 
-    def do_depend(self, args):
-        return subprocess.check_output('poetry export -f requirements.txt --output requirements.txt', shell=True, text=True)
+# PYTHON
 
-    def do_filt_depend(self, args):
+    def copy_main_file(self, path):
+        """
+        Copy main.{path}.py file corresponding to Python build stage
+        """
+        print('>> preparing main.py')
+        shutil.copy(f'main.{path}.py', 'main.py')
+
+    def do_main_group(self, args):
+        """
+        Compile front-end and deploy to App Engine. Deploy primary functions (run_user_playlist, update_tag)
+        """
+        self.do_set_project(None)
+        self.export_filtered_dependencies()
+
+        self.do_app(args)
+        self.do_tag(None)
+        self.do_playlist(None)
+
+    def do_app(self, args):
+        """
+        Compile front-end and deploy to App Engine
+        """
+        if not '-nb' in args.strip().split(' '):
+            print(">> compiling frontend")
+            self.compile_frontend()
+        self.copy_main_file('api')
+
+        print('>> deploying app engine service')
+        subprocess.check_call('gcloud app deploy', shell=True)
+
+    def function_deploy(self, main, function_id):
+        self.copy_main_file(main)
+
+        print(f'>> deploying {function_id}')
+        self.deploy_function(function_id)
+
+    def do_tag(self, args):
+        """
+        Deploy update_tag function
+        """
+        self.function_deploy('update_tag', 'update_tag')
+
+    def do_playlist(self, args):
+        """
+        Deploy run_user_playlist function
+        """
+        self.function_deploy('run_playlist', 'run_user_playlist')
+
+
+    # all playlists cron job
+    def do_playlist_cron(self, args):
+        """
+        Deploy run_all_playlists function
+        """
+        self.function_deploy('cron', 'run_all_playlists')
+
+    # all stats refresh cron job
+    def do_playlist_stats_cron(self, args):
+        """
+        Deploy run_all_playlist_stats function
+        """
+        self.function_deploy('cron', 'run_all_playlist_stats')
+
+    # all tags cron job
+    def do_tags_cron(self, args):
+        """
+        Deploy run_all_tags function
+        """
+        self.function_deploy('cron', 'run_all_tags')
+
+    # redeploy all cron job functions
+    def do_cron_functions(self, args):
+        """
+        Deploy background functions including cron job scheduling for update actions (run_all_playlists, run_all_playlist_stats, run_all_tags)
+        """
+        self.do_set_project(None)
+        self.export_filtered_dependencies()
+        
+        self.do_playlist_cron(None)
+        self.do_playlist_stats_cron(None)
+        self.do_tags_cron(None)
+
+    def do_pydepend(self, args):
+        """
+        Generate and export requirements.txt from Poetry manifest
+        """
         self.export_filtered_dependencies()
 
     def export_filtered_dependencies(self):
@@ -190,6 +208,38 @@ class Admin(Cmd):
 
         with open('requirements.txt', 'w') as f:
             f.write("\n".join(filtered))
+
+# FRONT-END
+
+    def compile_frontend(self):
+        """
+        Compile sass to css and run npm build task
+        """
+        print('>> building css')
+        subprocess.check_call(f'sass --style=compressed {str(scss_rel_path)} {str(css_rel_path)}', shell=True)
+
+        print('>> building javascript')
+        subprocess.check_call('npm run build', shell=True)
+
+    def do_sass(self, args):
+        """
+        Compile sass to css
+        """
+        subprocess.check_call(f'sass --style=compressed {str(scss_rel_path)} {str(css_rel_path)}', shell=True)
+
+    def do_watchsass(self, args):
+        """
+        Run sass compiler with watch argument to begin watching source folder for changes
+        """
+        subprocess.check_call(f'sass --style=compressed --watch {str(scss_rel_path)} {str(css_rel_path)}', shell=True)
+
+    def do_exit(self, args):
+        """
+        Exit script
+        """
+        exit(0)
+
+
 
 def test():
     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'service.json'
